6f79dbc8e6a14c87bdc0981553fe1548d93fd930
Mohd Saquib <nsaquib96@gmail.com>
2021-03-16 00:13:11 -0400

    Added bootstrap to headerbody

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import Header from './Components/Header/Header';
 
 class App extends Component {
   state = {
-    Header: [
-      { name: 'Mohd Nazmus Saquib' },
-      { title: 'Cloud Solution Architect' },
-      { location: 'Washington DC, USA' },
-      { image: 'profile' },
-      {
-        content: '👋 Hello there! My name is Mohd Saquib and I have a passion for Cloud Computing ☁️, Data Analytics 🗄️ and all the Developer stuff 📟! I am currently pursuing a Masters Degree in Information Technology 🚀 from George Mason University 🏫. <br> <br> I am constantly learning new cloud and container technologies to become a better cloud developer ☁️. I was recently endored by Forrest Brazzel (AWS Serverless Hero) for AWS on LinkedIn for my cloud projects. I am also actively learning JAVA and Serverless Framwork to work on my next big porject! I enjoy playing FPS games 🎮 mainly CS:GO and Call of Duty 🕹️, so if you are interested in competing against me, feel free to hit me up 👍! <br><br> I speak four languages fluently 🇺🇸 🇧🇩 🇮🇳 🇵🇰 and know a little bit of Spanish 🇪🇸. I’m interested in just about any topic and very much open to thoughtful conversations. 🍺 Hit me up at <a href="mailto:nsaquib96@gmail.com" style="text-decoration: dotted underline; color: white;">nsaquib96@gmail.com</a> and let’s chat about stuff!'
-      }
-    ]
+    Header: {
+      name: 'Mohd Nazmus Saquib',
+      title: 'Cloud Solution Architect',
+      location: 'Washington DC, USA',
+      image: 'profile',
+      content: ["👋 Hello there! My name is Mohd Saquib and I have a passion for Cloud Computing ☁️, Data Analytics 🗄️ and all the Developer stuff 📟! I am currently pursuing a Masters Degree in Information Technology 🚀 from George Mason University 🏫.", <br></br>, <br></br>, "I am constantly learning new cloud and container technologies to become a better cloud developer ☁️. I was recently endored by Forrest Brazzel (AWS Serverless Hero) for AWS on LinkedIn for my cloud projects. I am also actively learning JAVA and Serverless Framwork to work on my next big porject! I enjoy playing FPS games 🎮 mainly CS:GO and Call of Duty 🕹️, so if you are interested in competing against me, feel free to hit me up 👍!", <br></br>, <br></br>, "I speak four languages fluently 🇺🇸 🇧🇩 🇮🇳 🇵🇰 and know a little bit of Spanish 🇪🇸. I’m interested in just about any topic and very much open to thoughtful conversations. 🍺 Hit me up at ", <a href='mailto: nsaquib96@gmail.com'>nsaquib96@gmail.com</a>, " and let’s chat about stuff!"]
+    }
   }
+
   render() {
     return (
       <div className="App">
         <Header
-          name={this.state.Header[0].name}
-          title={this.state.Header[1].title}
-          location={this.state.Header[2].location}
-          image={this.state.Header[3].image}
-          content={this.state.Header[4].content} />
+          name={this.state.Header.name}
+          title={this.state.Header.title}
+          location={this.state.Header.location}
+          image={this.state.Header.image}
+          content={this.state.Header.content}
+        />
       </div>
     );
   }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,26 +1,32 @@
 import React from 'react';
 
 import './Header.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
 
 const Header = (props) => {
     return (
         <div>
-            <div class="container-fluid-2">
-                <div class="row">
-                    <div class="col-md-4 index-about-section">
+            <Container className="p-5">
+                <Row>
+                    <Col md={4} className="index-about-section">
                         <div align="center">
                             <img class="index-about-image" src={require(`./assets/${props.image}.png`)} />
                             <h1 class="index-about-name">{props.name}</h1>
                             <h5 class="about-jobTitle">{props.title}</h5>
                             <h6 class="about-location"><i class="fas fa-map-marker-alt"></i>{props.location}</h6>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-8  index-about-section">
-                <p>{props.content}</p>
-            </div>
+                    </Col>
+                    <Col md={8} className="index-about-section">
+                        <p className="index-about-section-p">{props.content}</p>
+                    </Col>
+                </Row>
+            </Container>
         </div>
+
     )
 };
 
