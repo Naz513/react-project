00e5609f351f27742be8f248101472a3ac28cea1
Mohd Saquib <nsaquib96@gmail.com>
2021-03-15 23:11:31 -0400

    Added Header Content section

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './Header.css'
+
 const Header = (props) => {
     return (
         <div>
@@ -7,13 +9,16 @@ const Header = (props) => {
                 <div class="row">
                     <div class="col-md-4 index-about-section">
                         <div align="center">
-                            <img class="index-about-image" src="../assets/profile.png"></img>
+                            <img class="index-about-image" src={require(`./assets/${props.image}.png`)} />
                             <h1 class="index-about-name">{props.name}</h1>
                             <h5 class="about-jobTitle">{props.title}</h5>
                             <h6 class="about-location"><i class="fas fa-map-marker-alt"></i>{props.location}</h6>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div class="col-md-8  index-about-section">
+                <p>{props.content}</p>
             </div>
         </div>
     )
